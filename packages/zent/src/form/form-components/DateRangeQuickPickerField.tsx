480b408be8c7931cec505bf09ecacb1a90155e5b
intellild <intellild@outlook.com>
2019-07-17 16:22:38 +0800

    date pickers form fields

--- packages/zent/src/form/form-components/DateRangeQuickPickerField.tsx
+++ packages/zent/src/form/form-components/DateRangeQuickPickerField.tsx
@@ -4,36 +4,23 @@ import DateRangeQuickPicker, {
   IDateRangeQuickPickerProps,
 } from '../../date-range-quick-picker';
 import { DatePickers } from '../../datetimepicker/common/types';
-import {
-  useField,
-  noopMapEventToValue,
-  IFormComponentCommonProps,
-  IFormFieldModelProps,
-} from '../shared';
+import { IFormComponentProps, dateRangeDefaultValueFactory } from '../shared';
 import { FormField } from '../Field';
 
-export interface IFormDateRangeQuickPickerFieldProps
-  extends IFormComponentCommonProps<
-    DatePickers.RangeValue,
-    Omit<IDateRangeQuickPickerProps, 'value'>
-  > {}
-
-function dateDefaultValueFactory(): DatePickers.RangeValue {
-  return [new Date(), new Date()];
-}
+export type IFormDateRangeQuickPickerFieldProps = IFormComponentProps<
+  DatePickers.RangeValue,
+  Omit<IDateRangeQuickPickerProps, 'value'>
+>;
 
 export const FormDateRangeQuickPickerField: React.FunctionComponent<
-  IFormDateRangeQuickPickerFieldProps &
-    IFormFieldModelProps<DatePickers.RangeValue>
+  IFormDateRangeQuickPickerFieldProps
 > = props => {
-  const [childProps, { error }, ref] = useField<
-    DatePickers.RangeValue,
-    DatePickers.RangeValue,
-    IDateRangeQuickPickerProps
-  >(props, dateDefaultValueFactory, noopMapEventToValue);
   return (
-    <FormField ref={ref} {...props} error={error}>
-      <DateRangeQuickPicker {...props.props} {...childProps} />
+    <FormField
+      {...props}
+      defaultValue={props.defaultValue || dateRangeDefaultValueFactory}
+    >
+      {childProps => <DateRangeQuickPicker {...props.props} {...childProps} />}
     </FormField>
   );
 };
